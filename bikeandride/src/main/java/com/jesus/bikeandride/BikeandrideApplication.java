e471e5d6878d07e10e96646ddff8e0f4e11041ea
JesusGonzalez82 <jgblanco82@yahoo.es>
2025-06-18 19:11:14 +0200

    commit

---
+++ bikeandride/src/main/java/com/jesus/bikeandride/BikeandrideApplication.java
@@ -0,0 +1,13 @@
+package com.jesus.bikeandride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeandrideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeandrideApplication.class, args);
+	}
+
+}
---
+++ bikeandride/src/test/java/com/jesus/bikeandride/BikeandrideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesus.bikeandride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeandrideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
