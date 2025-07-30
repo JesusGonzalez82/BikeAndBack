706012ff4f4cf94f288d24f53c4a119fa1f7e61b
JesusGonzalez82 <jgblanco82@yahoo.es>
2025-07-30 19:25:39 +0200

    listUser

--- bikeandride/src/main/java/Controller/UserController.java
+++ bikeandride/src/main/java/Controller/UserController.java
@@ -1,4 +1,20 @@
 package Controller;
 
+import model.UserDdbb;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    @GetMapping("/users/listaUsers/")
+    public List<UserDdbb> getUsers(){
+        List<UserDdbb> list = new ArrayList<>();
+        list.add(new UserDdbb());
+        return list;
+    }
+
 }
--- bikeandride/src/main/java/model/User.java
+++ bikeandride/src/main/java/model/User.java
@@ -1,4 +1,69 @@
 package model;
 
+import java.util.Date;
+
 public class User {
+
+    // name
+    // password
+    // id
+    // email
+    // birthday
+
+    // Esto es un DTO y lo que traemos de la BBDD es un DAO
+
+    private String name;
+    private String password;
+    private int id;
+    private String email;
+    private Date birthday;
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
 }
