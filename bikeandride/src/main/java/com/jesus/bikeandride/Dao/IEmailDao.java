4f7e0bf20b531fbaa4c7eae4e05044a9712056e2
JesusGonzalez82 <jgblanco82@yahoo.es>
2025-09-08 06:53:41 +0200

    Desarrollando WS Emai

--- bikeandride/src/main/java/com/jesus/bikeandride/Dao/IEmailDao.java
+++ bikeandride/src/main/java/com/jesus/bikeandride/Dao/IEmailDao.java
@@ -1,4 +1,10 @@
 package com.jesus.bikeandride.Dao;
 
+import com.jesus.bikeandride.Model.BikeDdbb;
+import com.jesus.bikeandride.Model.EmailDdbb;
+
+import java.util.List;
+
 public interface IEmailDao {
+    List<EmailDdbb> getListEmailByUserId(long idUser);
 }
