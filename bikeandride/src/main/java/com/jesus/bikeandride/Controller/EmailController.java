db309eb568e3cd765099ccd6a5d1ab28ea0d06b4
JesusGonzalez82 <jgblanco82@yahoo.es>
2025-09-08 06:53:41 +0200

    Desarrollando WS Emai

--- bikeandride/src/main/java/com/jesus/bikeandride/Controller/EmailController.java
+++ bikeandride/src/main/java/com/jesus/bikeandride/Controller/EmailController.java
@@ -1,4 +1,32 @@
 package com.jesus.bikeandride.Controller;
 
+import com.jesus.bikeandride.Dao.EmailDao;
+import com.jesus.bikeandride.Model.EmailDdbb;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/emails")
 public class EmailController {
+
+        private final List<EmailDdbb> emails = new ArrayList<>();
+        @Autowired
+        private EmailDao emailDao;
+
+        @GetMapping("/listaEmails/")
+        public List<EmailDdbb> getEmails(){
+            return  emails;
+        }
+
+        @GetMapping("/getListaEmailByUserId/{id}")
+        public List<EmailDdbb> getListEmailByUserId(@PathVariable long id){
+            return emailDao.getListEmailByUserId(id);
+        }
+
 }
