56bcdfbdde3f6fd756e183600ad430543386508b
JesusGonzalez82 <jgblanco82@yahoo.es>
2025-09-09 07:14:54 +0200

    Modificaciones para que los métodos devuelvan True o False

--- bikeandride/src/main/java/com/jesus/bikeandride/Controller/EmailController.java
+++ bikeandride/src/main/java/com/jesus/bikeandride/Controller/EmailController.java
@@ -1,12 +1,10 @@
 package com.jesus.bikeandride.Controller;
 
 import com.jesus.bikeandride.Dao.EmailDao;
+import com.jesus.bikeandride.Model.Email;
 import com.jesus.bikeandride.Model.EmailDdbb;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,18 +13,53 @@ import java.util.List;
 @RequestMapping("/emails")
 public class EmailController {
 
-        private final List<EmailDdbb> emails = new ArrayList<>();
-        @Autowired
-        private EmailDao emailDao;
+    @Autowired
+    private EmailDao emailDao;
 
-        @GetMapping("/listaEmails/")
-        public List<EmailDdbb> getEmails(){
-            return  emails;
-        }
+    @GetMapping("/exists/id/{id}")
+    public boolean emailExistsById(@PathVariable long id) {
+        return emailDao.emailExistsById(id);
+    }
 
-        @GetMapping("/getListaEmailByUserId/{id}")
-        public List<EmailDdbb> getListEmailByUserId(@PathVariable long id){
-            return emailDao.getListEmailByUserId(id);
-        }
+    @GetMapping("/exists/email/{email}")
+    public boolean emailExistsByAddress(@PathVariable String email) {
+        return emailDao.emailExistsByAddress(email);
+    }
+
+    @GetMapping("/user/{userId}/hasEmails")
+    public boolean userHasEmail(@PathVariable long userId) {
+        return emailDao.userHasEmails(userId);
+    }
+
+    @GetMapping("/belongs/{emails}/users/{userId}")
+    public boolean emailBelongsToUser(@PathVariable long emailId, @PathVariable long userId) {
+        return emailDao.emailBelongsToUser(emailId, userId);
+    }
+
+    @PostMapping("/create")
+    public boolean createEmail(@RequestBody EmailDdbb newEmail) {
+        return emailDao.createEmail(newEmail);
+    }
+
+    @PostMapping("/update")
+    public boolean updateEmail(@RequestBody EmailDdbb email) {
+        return emailDao.updateEmail(email);
+    }
+
+    @DeleteMapping("/delete/id/{id}")
+    public boolean deleteEmailById(@PathVariable long id) {
+        return emailDao.deleteEmailById(id);
+    }
+
+    @DeleteMapping("/delete/email/{email}")
+    public boolean deleteEmailByAddress(@PathVariable String Email) {
+        return emailDao.deleteEmailByAddress(email);
+    }
+
+    //     Mantenemos el método que devuelve la lista para los casos en los que tenemos que comprobar los datos
+    @GetMapping("/user/{userId}/list")
+    public List<EmailDdbb> getEmailByUser(@PathVariable long userId) {
+        return emailDao.getListEmailByUserId(userId);
+    }
 
 }
