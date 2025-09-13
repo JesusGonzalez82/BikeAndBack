17de647bbd372bbe01a22ffd7e1dc0167f43c543
JesusGonzalez82 <jgblanco82@yahoo.es>
2025-09-13 09:37:14 +0200

    Termino con los test unitarios de las bicicletas, con una cobertura del 89% de las lineas

--- bikeandride/src/test/java/com/jesus/bikeandride/Controller/BikeControllerTest.java
+++ bikeandride/src/test/java/com/jesus/bikeandride/Controller/BikeControllerTest.java
@@ -38,6 +38,30 @@ public class BikeControllerTest {
     }
 
     @Test
+    @DisplayName("Debe crear una nueva bicicleta")
+    void createBike(){
+        BikeDdbb newBike = createTestBike(1L, "Giant", "mtb");
+        BikeDdbb createdBike = createTestBike(1L, "Giant", "mtb");
+
+        when(bikeDao.createBike(newBike)).thenReturn(createdBike);
+
+        BikeDdbb resultado = bikeController.createBike(newBike);
+
+        assertNotNull(resultado);
+        assertEquals(1L, resultado.getIdBike());
+        verify(bikeDao, times(1)).createBike(newBike);
+    }
+
+    @Test
+    @DisplayName("Debe obtener el listado de todas las bicicletas")
+    void getAllBikes(){
+        List<BikeDdbb> resultado = bikeController.getAllBikes();
+
+        assertNotNull(resultado);
+        assertTrue(resultado.isEmpty());
+    }
+
+    @Test
     @DisplayName("Debe obtener la lista de bicicletas por el id de usuario")
     void getListBikeByUserId(){
         Long userId = 1L;
@@ -72,7 +96,91 @@ public class BikeControllerTest {
         verify(bikeDao, times(1)).getListBikeByUserId(userId);
     }
 
-    
+    @Test
+    @DisplayName("Debe devolver una lista vacia cuando un usuario no tenga bicicletas")
+    void getListBikeWhenUserNoBikes(){
+        Long userId = 666L;
+        when(bikeDao.getListBikeByUserId(userId)).thenReturn(Arrays.asList());
+        // When
+        List<BikeDdbb> resultado = bikeController.getListBikeByUserId(userId);
+        //Then
+        assertNotNull(resultado);
+        assertTrue(resultado.isEmpty());
+        verify(bikeDao, times(1)).getListBikeByUserId(userId);
+    }
+
+    @Test
+    @DisplayName("Actualizar varios campos a la vez")
+    void partialUpdateBike(){
+        Long bikeId = 1L;
+        Map<String, Object> updates = new HashMap<>();
+        updates.put("tipo", "Gravel");
+        updates.put("anio", 2024);
+        updates.put("status", "en uso");
+
+        BikeDdbb existingBike = createTestBike(bikeId, "Canyon", "road");
+        BikeDdbb updateBike = createTestBike(bikeId, "Canyon", "mtb");
+        updateBike.setBirthday(Year.of(2024));
+        updateBike.setStatus("en mantenimiento");
+
+        when(bikeDao.getBikeById(bikeId)).thenReturn(existingBike);
+        when(bikeDao.updateBike(existingBike)).thenReturn(updateBike);
+
+        BikeDdbb resultado = bikeController.partialUpdateBike(bikeId, updates);
+
+        assertNotNull(resultado);
+        assertEquals("mtb", resultado.getType());
+        assertEquals(Year.of(2024), resultado.getBirthday());
+        assertEquals("en mantenimiento", resultado.getStatus());
+
+        verify(bikeDao, times(1)).getBikeById(bikeId);
+        verify(bikeDao, times(1)).updateBike(existingBike);
+
+    }
+
+    @Test
+    @DisplayName("Lanzar una excepción cuando la bicicleta no existe")
+    void partialUpdateBikeWhitNotExists(){
+        Long bikeId = 666L;
+        Map<String, Object> updates = new HashMap<>();
+        updates.put("tipo", "mtb");
+
+        when(bikeDao.getBikeById(bikeId)).thenReturn(null);
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () ->{
+            bikeController.partialUpdateBike(bikeId, updates);
+        });
+
+        assertEquals("Error al actualizar la bicicleta: Bicicleta no encontrada", exception.getMessage());
+
+        verify(bikeDao, times(1)).getBikeById(bikeId);
+        verify(bikeDao, never()).updateBike(any(BikeDdbb.class));
+    }
+
+    @Test
+    @DisplayName("Errores al insertar campos no reconocidos")
+    void partialUpdateBikeNoRecognicedFields(){
+        Long bikeId = 1L;
+        Map<String, Object> updates = new HashMap<>();
+        updates.put("tipo", "E-bike");
+        updates.put("campoInexistente", "valor");
+        updates.put("fabricacion", 101);
+
+        BikeDdbb existingBike = createTestBike(bikeId, "Citroen", "road");
+        BikeDdbb updateBike = createTestBike(bikeId, "citroen", "E-bike");
+
+        when(bikeDao.getBikeById(bikeId)).thenReturn(existingBike);
+        when(bikeDao.updateBike(existingBike)).thenReturn(updateBike);
+
+        BikeDdbb resultado = bikeController.partialUpdateBike(bikeId, updates);
+
+        assertNotNull(resultado);
+        assertEquals("E-bike", resultado.getType());
+
+        verify(bikeDao, times(1)).getBikeById(bikeId);
+        verify(bikeDao, times(1)).updateBike(existingBike);
+    }
+
 
     private BikeDdbb createTestBike(Long id, String marca, String tipo) {
         BikeDdbb bike = new BikeDdbb();
